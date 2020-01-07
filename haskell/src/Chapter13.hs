213762762650881c1b8e4b8fac3b94a791620b72
Flavio Corpa <flavio.corpa@47deg.com>
2020-01-07 09:07:50 +0100

    Finish Chapter 13 exercise! 🧬

--- haskell/src/Chapter13.hs
+++ haskell/src/Chapter13.hs
@@ -46,13 +46,58 @@ data Foo a b c
 instance (Eq a, Eq b, Eq c) => Eq (Foo a b c) where
   (==) = genericEq
 
--- TODO: exercise 13.2-i
+instance (Ord a, Ord b, Ord c) => Ord (Foo a b c) where
+  compare = genericOrd
+
+-- exercise 13.2-i
 -- provide a generic instance for the Ord class.
 class GOrd a where
-  gcompare :: a x -> a x -> Ordering
+  gord :: a x -> a x -> Ordering
 
--- TODO: exercise 13.2-ii
+instance GOrd U1 where -- () -- empty data constructor
+  gord _ _ = EQ
+
+instance GOrd V1 where -- Void
+  gord _ _ = EQ
+
+instance Ord a => GOrd (K1 _1 a) where -- type inside a data constructor
+  gord (K1 a) (K1 b) = compare a b
+
+instance (GOrd a, GOrd b) => GOrd (a :+: b) where -- sum types!
+  gord (L1 a1) (L1 a2) = gord a1 a2
+  gord (R1 b1) (R1 b2) = gord b1 b2
+  gord (L1 _) (R1 _) = LT
+  gord (R1 _) (L1 _) = GT
+
+instance (GOrd a, GOrd b) => GOrd (a :*: b) where -- product types!
+  gord (a1 :*: b1) (a2 :*: b2) = gord a1 a2 <> gord b1 b2
+
+instance GOrd a => GOrd (M1 _x _y a) where -- metadata
+  gord (M1 a1) (M1 a2) = gord a1 a2
+
+genericOrd :: (Generic a, GOrd (Rep a)) => a -> a -> Ordering
+genericOrd a b = gord (from a) (from b)
+
+-- exercise 13.2-ii
 -- implement the function exNihilo :: Maybe a with GHC.Generics
--- Just a if a is one data constructor which takes zero arguments, Nothing otherwise
+-- `Just a` if a is one data constructor which takes zero arguments, Nothing otherwise
 class GexNihilo a where
   gexNihilo :: Maybe (a x)
+
+instance GexNihilo U1 where
+  gexNihilo = Just U1
+
+instance GexNihilo V1 where
+  gexNihilo = Nothing
+
+instance GexNihilo (K1 _x a) where
+  gexNihilo = Nothing
+
+instance GexNihilo (a :+: b) where
+  gexNihilo = Nothing
+
+instance GexNihilo (a :*: b) where
+  gexNihilo = Nothing
+
+instance GexNihilo a => GexNihilo (M1 _x _y a) where -- metadata
+  gexNihilo = fmap M1 gexNihilo
