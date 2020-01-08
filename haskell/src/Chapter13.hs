ad9edbe41af2222876622379a210148c95e77893
Flavio Corpa <flavio.corpa@47deg.com>
2020-01-08 16:59:38 +0100

    Finish Chapter 13 on Generics 🤯🤯🤯

--- haskell/src/Chapter13.hs
+++ haskell/src/Chapter13.hs
@@ -1,13 +1,34 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -O -fplugin Test.Inspection.Plugin #-}
 
 module Chapter13 where
 
+import Control.Monad.Writer
+import Data.Aeson ((.=), ToJSON, Value (..), object)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import qualified Data.ByteString.Lazy.Char8 as LC8
+import Data.Kind (Type)
+import Data.Text (Text, pack)
+import Data.Typeable
+import Data.Vector (fromList)
 import GHC.Generics
+import GHC.TypeLits
+import qualified GHC.TypeLits as Err
+import Test.Inspection
 
 -- let's write a generic Eq instance!
 class GEq a where
@@ -119,3 +140,165 @@ data Person
         permissions :: [Bool]
       }
   deriving (Generic)
+
+class GJSONSchema (a :: Type -> Type) where
+  gschema :: Writer [Text] Value
+
+mergeObjects :: Value -> Value -> Value
+mergeObjects (Object a) (Object b) = Object (a <> b)
+
+emitRequired :: forall nm. KnownSymbol nm => Writer [Text] ()
+emitRequired = tell . pure . pack . symbolVal $ Proxy @nm
+
+type family RepName (x :: Type -> Type) :: Symbol where
+  RepName (D1 ('MetaData nm _ _ _) _) = nm
+
+type family TypeName (t :: Type) :: Symbol where
+  TypeName t = RepName (Rep t)
+
+type family ToJSONType (a :: Type) :: Symbol where
+  ToJSONType Int = "integer"
+  ToJSONType Integer = "integer"
+  ToJSONType Float = "number"
+  ToJSONType Double = "number"
+  ToJSONType String = "string"
+  ToJSONType Bool = "boolean"
+  ToJSONType [a] = "array"
+  ToJSONType a = TypeName a
+
+makeTypeObj :: forall a. KnownSymbol (ToJSONType a) => Value
+makeTypeObj = object ["type" .= String (pack . symbolVal $ Proxy @(ToJSONType a))]
+
+makePropertyObj :: forall name. KnownSymbol name => Value -> Value
+makePropertyObj v = object [pack (symbolVal $ Proxy @name) .= v]
+
+instance
+  (KnownSymbol nm, KnownSymbol (ToJSONType a)) =>
+  GJSONSchema (M1 S ('MetaSel ('Just nm) _1 _2 _3) (K1 _4 a))
+  where
+  gschema = do
+    emitRequired @nm
+    pure . makePropertyObj @nm $ makeTypeObj @a
+  {-# INLINE gschema #-}
+
+instance (GJSONSchema f, GJSONSchema g) => GJSONSchema (f :*: g) where
+  gschema =
+    mergeObjects
+      <$> gschema @f
+      <*> gschema @g
+  {-# INLINE gschema #-}
+
+instance
+  (TypeError ('Err.Text "JSON Schema does not support sum types")) =>
+  GJSONSchema (f :+: g)
+  where
+  gschema = error "JSON Schema does not support sum types"
+  {-# INLINE gschema #-}
+
+instance GJSONSchema a => GJSONSchema (M1 C _1 a) where
+  gschema = gschema @a
+  {-# INLINE gschema #-}
+
+instance
+  (GJSONSchema a, KnownSymbol nm) =>
+  GJSONSchema (M1 D ('MetaData nm _1 _2 _3) a)
+  where
+  gschema = do
+    sch <- gschema @a
+    pure $
+      object
+        [ "title" .= (String . pack . symbolVal $ Proxy @nm),
+          "type" .= String "object",
+          "properties" .= sch
+        ]
+  {-# INLINE gschema #-}
+
+schema :: forall a. (GJSONSchema (Rep a), Generic a) => Value
+schema =
+  let (v, reqs) = runWriter $ gschema @(Rep a)
+   in mergeObjects v $ object ["required" .= Array (fromList $ String <$> reqs)]
+{-# INLINE schema #-}
+
+instance
+  {-# OVERLAPPING #-}
+  ( KnownSymbol nm,
+    KnownSymbol (ToJSONType a)
+  ) =>
+  GJSONSchema (M1 S ('MetaSel ('Just nm) _1 _2 _3) (K1 _4 (Maybe a)))
+  where
+  gschema = pure . makePropertyObj @nm $ makeTypeObj @a
+  {-# INLINE gschema #-}
+
+instance
+  {-# OVERLAPPING #-}
+  ( KnownSymbol nm,
+    KnownSymbol (ToJSONType [a]),
+    KnownSymbol (ToJSONType a)
+  ) =>
+  GJSONSchema (M1 S ('MetaSel ('Just nm) _1 _2 _3) (K1 _4 [a]))
+  where
+  gschema = do
+    emitRequired @nm
+    let innerType = object ["items" .= makeTypeObj @a]
+    pure . makePropertyObj @nm . mergeObjects innerType $ makeTypeObj @[a]
+  {-# INLINE gschema #-}
+
+instance
+  {-# OVERLAPPING #-}
+  (KnownSymbol nm) =>
+  GJSONSchema (M1 S ('MetaSel ('Just nm) _1 _2 _3) (K1 _4 String))
+  where
+  gschema = do
+    emitRequired @nm
+    pure . makePropertyObj @nm $ makeTypeObj @String
+  {-# INLINE gschema #-}
+
+pp :: ToJSON a => a -> IO ()
+pp = LC8.putStrLn . encodePretty
+
+-- * Chapter13> pp (schema @Person) 🎉🎉🎉
+-- {
+--     "required": [
+--         "name",
+--         "age",
+--         "permissions"
+--     ],
+--     "title": "Person",
+--     "type": "object",
+--     "properties": {
+--         "phone": {
+--             "type": "string"
+--         },
+--         "age": {
+--             "type": "integer"
+--         },
+--         "name": {
+--             "type": "string"
+--         },
+--         "permissions": {
+--             "items": {
+--                 "type": "boolean"
+--             },
+--             "type": "array"
+--         }
+--     }
+-- }
+
+-- performance
+
+mySchema :: Value
+mySchema = schema @Person
+
+inspect $ hasNoGenerics 'mySchema
+
+-- Kan Extensions 🤯🤯🤯
+newtype Yoneda f a
+  = Yoneda
+      { runYoneda :: forall b. (a -> b) -> f b
+      }
+
+-- Data.Functor.Yoneda == forall f. Functor f => f a
+-- Data.Functor.Day.Curried == forall f. Applicative f => f a
+-- Control.Monad.Codensity == forall f. Monad f => f a
+instance Functor (Yoneda f) where
+  fmap f (Yoneda y) = Yoneda $ \k -> y (k . f)
