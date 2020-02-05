639803cf0c007b6392a1a8eaf23317594d123758
Flavio Corpa <flavio.corpa@47deg.com>
2020-02-05 11:50:04 +0100

    Implementation of a loggin monad 🐒

--- haskell/src/Chapter15.hs
+++ haskell/src/Chapter15.hs
@@ -37,3 +37,42 @@ withSomeSBool (SomeSBool s) f = f s
 toSBool :: Bool -> SomeSBool
 toSBool True = SomeSBool STrue
 toSBool False = SomeSBool SFalse
+
+-- ad-hoc implementation
+
+class Monad (LogginMonad b)
+    => MonadLogging (b :: Bool) where
+  type LogginMonad b = (r :: Type -> Type) | r -> b
+  logMsg :: String -> LogginMonad b ()
+  runLogging :: LogginMonad b a -> IO a
+
+instance MonadLogging 'False where
+  type LogginMonad 'False = IO
+  logMsg _ = pure ()
+  runLogging = id
+
+instance MonadLogging 'True where
+  type LogginMonad 'True = WriterT [String] IO
+  logMsg s = tell [s]
+  runLogging m = do
+    (a, w) <- runWriterT m
+    for_ w putStrLn
+    pure a
+
+program :: MonadLogging b => LogginMonad b ()
+program = do
+  logMsg "hello world"
+  pure ()
+
+main :: IO ()
+main = do
+  bool <- read <$> getLine
+  withSomeSBool (toSBool bool) $ \(sb :: SBool b) ->
+    case dict @MonadLogging sb of
+      Dict -> runLogging @b program
+
+dict :: (c 'True, c 'False) => SBool b -> Dict (c b)
+dict STrue = Dict
+dict SFalse = Dict
+
+-- generalized machinery
