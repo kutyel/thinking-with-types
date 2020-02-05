04f801a035b2d438fa9023c353985191110b4cb9
Flavio Corpa <flavio.corpa@47deg.com>
2020-02-05 11:28:54 +0100

    Start chapter 15

--- haskell/src/Chapter15.hs
+++ haskell/src/Chapter15.hs
@@ -1,3 +1,39 @@
+{-# language ConstraintKinds #-}
+{-# language DataKinds #-}
+{-# language FlexibleContexts #-}
+{-# language GADTs #-}
+{-# language RankNTypes #-}
+{-# language ScopedTypeVariables #-}
+{-# language TypeApplications #-}
+{-# language TypeFamilies #-}
+{-# language TypeFamilyDependencies #-}
+
 module Chapter15 where
 
 -- Dependent Types 🤯
+
+import Control.Monad.Trans.Writer
+import Data.Constraint (Dict (..))
+import Data.Foldable (for_)
+import Data.Kind (Type)
+
+data SBool (b :: Bool) where
+  STrue :: SBool 'True
+  SFalse :: SBool 'False
+
+fromSBool :: SBool b -> Bool
+fromSBool STrue = True
+fromSBool SFalse = False
+
+data SomeSBool where
+  SomeSBool :: SBool b -> SomeSBool
+
+withSomeSBool
+  :: SomeSBool
+  -> (forall (b :: Bool). SBool b -> r)
+  -> r
+withSomeSBool (SomeSBool s) f = f s
+
+toSBool :: Bool -> SomeSBool
+toSBool True = SomeSBool STrue
+toSBool False = SomeSBool SFalse
